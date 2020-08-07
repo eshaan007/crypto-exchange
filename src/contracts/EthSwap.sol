d96522d7cc7de6397b328cb5308b76003da85f3b
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-08-07 20:30:46 +0530

    Added edits

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "./Token.sol";
 
 contract EthSwap {
-  string public name = "EthSwap Instant Exchange";
+  string public name = "Crypto Exchange";
   Token public token;
   uint public rate = 100;
 
